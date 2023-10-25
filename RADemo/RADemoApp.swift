9cf9039832bb38847c1726f2a8f6398540c40153
Roberto Pastor <roberto.pastor@cabify.com>
2023-10-25 18:04:05 +0200

    Initial Commit

---
+++ RADemo/RADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RADemoApp.swift
+//  RADemo
+//
+//  Created by Roberto Pastor on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
