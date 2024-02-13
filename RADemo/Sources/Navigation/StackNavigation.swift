9417134f830bf544dfd0f1c607eb60ba1b28f68d
Roberto Pastor <roberto.pastor@cabify.com>
2024-02-13 16:46:07 +0100

    - Refactored StackNavigation with TCA 1.8.0

--- RADemo/Sources/Navigation/StackNavigation.swift
+++ RADemo/Sources/Navigation/StackNavigation.swift
@@ -24,61 +24,25 @@ public struct StackNavigation {
     public var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-            case let .path(.element(id: _, action: .gamesForSystem(.gameRowTapped(game)))):
-                print("Tapped \(game.title)")
-                state.path.append(.game(.init()))
-                return .none
             case .path:
                 return .none
             }
         }
-        .forEach(\.path, action: \.path) {
-            Path()
-        }
+        .forEach(\.path, action: \.path)
     }
     
-    @Reducer
-    public struct Path {
-        @ObservableState
-        public enum State: Equatable {
-            case achievement(AchievementFeature.State)
-            case game(GameFeature.State)
-            case gamesForSystem(GamesForSystemFeature.State)
-            case user(UserFeature.State)
-        }
-        
-        public enum Action {
-            case achievement(AchievementFeature.Action)
-            case game(GameFeature.Action)
-            case gamesForSystem(GamesForSystemFeature.Action)
-            case user(UserFeature.Action)
-        }
-        
-        public init() {}
-        
-        public var body: some ReducerOf<Self> {
-            Scope(state: \.achievement, action: \.achievement) {
-                AchievementFeature()
-            }
-            
-            Scope(state: \.game, action: \.game) {
-                GameFeature()
-            }
-            
-            Scope(state: \.gamesForSystem, action: \.gamesForSystem) {
-                GamesForSystemFeature()
-            }
-            
-            Scope(state: \.user, action: \.user) {
-                UserFeature()
-            }
-        }
+    @Reducer(state: .equatable)
+    public enum Path {
+        case achievement(AchievementFeature)
+        case game(GameFeature)
+        case gamesForSystem(GamesForSystemFeature)
+        case user(UserFeature)
     }
 }
 
 public struct StackNavigationView<Root: View>: View {
     
-    @State var store: StoreOf<StackNavigation>
+    @Bindable var store: StoreOf<StackNavigation>
     @ViewBuilder let root: () -> Root
     
     public init(
@@ -92,24 +56,16 @@ public struct StackNavigationView<Root: View>: View {
     public var body: some View {
         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
             root()
-        } destination: {
-            switch $0.state {
-            case .achievement:
-                if let store = $0.scope(state: \.achievement, action: \.achievement) {
-                    AchievementView(store: store)
-                }
-            case .game:
-                if let store = $0.scope(state: \.game, action: \.game) {
-                    GameView(store: store)
-                }
-            case .gamesForSystem:
-                if let store = $0.scope(state: \.gamesForSystem, action: \.gamesForSystem) {
-                    GamesForSystemView(store: store)
-                }
-            case .user:
-                if let store = $0.scope(state: \.user, action: \.user) {
-                    UserView(store: store)
-                }
+        } destination: { store in
+            switch store.case {
+            case let .achievement(store):
+                AchievementView(store: store)
+            case let .game(store):
+                GameView(store: store)
+            case let .gamesForSystem(store):
+                GamesForSystemView(store: store)
+            case let .user(store):
+                UserView(store: store)
             }
         }
     }
